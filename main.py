c7bfb296ef36728af6f3864217e80a87afe6f76c
izhgpi <johndoe@gmail.com>
2026-03-28 12:17:08 +0100

    Co-authored-by: Nathan-44 <Nathan-44@users.noreply.github.com> Co-authored-by: ThomasL0811 <ThomasL0811@users.noreply.github.com>

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ from bson.objectid import ObjectId
 load_dotenv()
 
 mongo = MongoClient(os.getenv('TOKEN'))
-films_collection = mongo.db.account.film
+films_collection = mongo.db.film
+db_users = mongo.db.account
+admin_collection = mongo.db.admin
 
 # Création de l'app
 app = Flask(__name__)
+
+app.secret_key = os.getenv("KEY")
 
 def utilisateur():
     """
@@ -63,16 +67,23 @@ def series():
 def tendances():
     return render_template("tendances.html", utilisateur=utilisateur())
 
-@app.route("/connexion")
+@app.route("/deconnexion")
+def deconnexion():
+    session.clear()
+    return redirect(url_for("index"))
+
+@app.route("/connexion", methods=["GET", "POST"])
 def connexion():
     #On vérifie si la méthode est POST pour traiter le formulaire reçu
     if request.method == 'POST':
-        #On récupère la table "email" de notre bdd
-        db_users = mongo.db.account
-        user=db_users.find_one({"email":request.form['email']})
+        if request.form["pseudo"] == "":
+            return render_template('connexion.html', error = "Merci d'entrer un pseudo.", utilisateur=utilisateur())
+
+        #On récupère la table "pseudo" de notre bdd
+        user=db_users.find_one({"pseudo":request.form['pseudo']})
         if user :
             if bcrypt.checkpw(request.form['mdp'].encode('utf-8'), user['mdp']):
-                session["utilisateur"] = request.form['utilisateur']
+                session["utilisateur"] = request.form['pseudo']
                 return redirect(url_for('index'))
         return render_template('connexion.html', error = 'Les identifiants ne sont pas reconnus.', utilisateur=utilisateur())
             
@@ -84,18 +95,24 @@ def creer_compte():
     if request.method == "GET":
         return render_template("createaccount.html", error=None, utilisateur=utilisateur())
     else :
+        if request.form["pseudo"] == "":
+            return render_template('connexion.html', error = "Merci d'entrer un pseudo.", utilisateur=utilisateur())
+        
         # On vérifie que les deux mdp sont les mêmes
         if request.form["mdp"] != request.form["mdp_confirm"]:
             print("Les deux mots de passe doivent être les mêmes !")
             return render_template("createaccount.html", error="Les deux mots de passe doivent être les mêmes !", utilisateur=utilisateur())
         
         # On vérifie si l'email existe déjà
-        email = request.form['email']
-        db_users = mongo.db.account
-        user=db_users.find_one({"email":email})
+        pseudo = request.form['pseudo']
+        user=db_users.find_one({"pseudo":pseudo})
         if user :
-            print("L'email est déjà enregistré !")
-            return render_template("createaccount.html", error="L'email est déjà enregistré !", utilisateur=utilisateur())
+            print("Le pseudo est déjà enregistré !")
+            return render_template("createaccount.html", error="Le pseudo est déjà enregistré !", utilisateur=utilisateur())
+        
+        # On vérifie si le mdp n'est pas vide
+        if request.form["mdp"] == "":
+            return render_template("createaccount.html", error="Le mot de passe doit contenir des crarctères !", utilisateur=utilisateur())
         
         # On encode le mdp
         mdp = request.form["mdp"].encode("utf-8")
@@ -103,10 +120,10 @@ def creer_compte():
         mdp_encrypt = bcrypt.hashpw(mdp, chaine_alea)
 
         # On l'ajoute à la bdd
-        db_users.insert_one({"email": email, "mdp":mdp_encrypt})
+        db_users.insert_one({"pseudo": pseudo, "mdp":mdp_encrypt})
 
         # On ajoute l'utilisateur à la session
-        session["utilisateur"] = request.form['utilisateur']
+        session["utilisateur"] = request.form['pseudo']
 
         return redirect(url_for('index'))
 
@@ -122,7 +139,14 @@ def film_page(id):
 @app.route("/testfilm")
 def testfilm():
     film = films_collection.find_one()
-    return str(film)
+    return render_template("exemplefilm.html", film=film)
+
+@app.route("/admin")
+def admin():
+    # On ne peut accéder à la page qui si l'utilisateur est connecté et est dans la liste des admins
+    if utilisateur() is not None and admin_collection.find_one({"pseudo": utilisateur()}) :
+        return render_template("admin.html", utilisateur=utilisateur())
+    return redirect(url_for("index"))
 
 # Gestion de l'erreur 404
 @app.errorhandler(404)
